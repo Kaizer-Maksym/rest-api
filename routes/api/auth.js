a54e50392971d2b3e5e005d0f711a2fe126ac450
Maksym Kaizer <kaizeriimaxim@gmail.com>
2022-11-05 14:59:23 +0100

    add user avatar

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,6 +4,7 @@ const { auth: ctrl } = require("../../controllers");
 
 const { ctrlWrapper } = require("../../utilities");
 const { authenticate } = require("../../middleWares");
+const { upload } = require("../../middleWares");
 
 const router = express.Router();
 
@@ -11,5 +12,11 @@ router.post("/register", ctrlWrapper(ctrl.register));
 router.post("/login", ctrlWrapper(ctrl.login));
 router.get("/current", authenticate, ctrlWrapper(ctrl.getCurrent));
 router.get("/logout", authenticate, ctrlWrapper(ctrl.logout));
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  ctrlWrapper(ctrl.updateAvatar)
+);
 
 module.exports = router;
